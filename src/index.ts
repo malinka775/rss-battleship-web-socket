0074983b5b926ff20f5797f1c7b9eba5bde40c91
Alina <malinka775@yandex.ru>
2024-10-27 23:09:05 +0200

    feat: add server import

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { httpServer } from "./http_server/index.js";
-// import { startWS } from "./ws_server/index.js";
+import { startWS } from "./ws_server/index.js";
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
-// startWS();
+startWS();
